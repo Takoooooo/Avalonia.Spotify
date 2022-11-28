5efd4154ccb2cecb95099d70a145a1dc0a17def8
Daniil Pavliuchyk <tako0qq@gmail.com>
2022-11-28 20:24:24 +0200

    Update to Avalonia 11.0.0-preview4

--- AvaloniaApplication14/AvaloniaApplication14.Android/MainActivity.cs
+++ AvaloniaApplication14/AvaloniaApplication14.Android/MainActivity.cs
@@ -1,16 +1,11 @@
 ﻿using Android.App;
 using Android.Content.PM;
-using Avalonia;
 using Avalonia.Android;
 
 namespace AvaloniaApplication14.Android
 {
-    [Activity(Label = "AvaloniaApplication14.Android", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon", LaunchMode = LaunchMode.SingleInstance, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
-    public class MainActivity : AvaloniaActivity<App>
+    [Activity(Label = "AvaloniaApplication14.Android", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon", LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
+    public class MainActivity : AvaloniaMainActivity
     {
-        protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
-        {
-            return base.CustomizeAppBuilder(builder);
-        }
     }
 }
--- AvaloniaApplication14/AvaloniaApplication14.Android/SplashActivity.cs
+++ AvaloniaApplication14/AvaloniaApplication14.Android/SplashActivity.cs
@@ -1,12 +1,24 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.OS;
+using Avalonia.Android;
 using Application = Android.App.Application;
 
 namespace AvaloniaApplication14.Android
 {
     [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
-    public class SplashActivity : Activity
+    public class SplashActivity : AvaloniaSplashActivity<App>
     {
+        protected override Avalonia.AppBuilder CustomizeAppBuilder(Avalonia.AppBuilder builder)
+        {
+            return base.CustomizeAppBuilder(builder);
+        }
+
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+
         protected override void OnResume()
         {
             base.OnResume();
